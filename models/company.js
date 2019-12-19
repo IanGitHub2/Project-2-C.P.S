df886718878ba85ff5f03b480d9d2b6e281a2a0e
Ian Kincaid <ikbusinessacct@gmail.com>
2019-12-18 21:12:18 -0500

    models finished for company

---
+++ models/company.js
@@ -0,0 +1,37 @@
+const mongoose = require('./connection.js')
+const companySchema = new mongoose.Schema({
+   name: String,
+   definition: String,
+   products: String,
+   est: Number 
+})
+
+const companyCollection = mongoose.model('company', companySchema)
+
+const getAllCompanys = () => {
+   return companyCollection.find({})
+}
+
+const getSingleCompany = (companyId) => {
+   return companyCollection.findOne({_companyId: companyId})
+}
+
+const addNewCompany = (newCompany) => {
+   return companyCollection.create(newCompany)
+}
+
+const editCompany = (companyId, editCompany) => {
+   return companyCollection.updateOne({_companyId: companyId}, editCompany)
+}
+
+const deleteCompany = (companyId) => {
+   return companyCollection.deleteOne({_companyId: companyId})
+}
+
+module.exports = {
+  getAllCompanys,
+  getSingleCompany,
+  addNewCompany,
+  editCompany,
+  deleteCompany
+}
